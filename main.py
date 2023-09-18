fd82743812086324bb37169d586da24018cac962
Sudhir Kumar <jakhalsudhir@gmail.com>
2023-09-18 17:01:57 +0530

    added pyinstaller executable python file

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-'''
-from PyPDF2 import *
-
-with open('table.pdf', 'rb')as f:
-    pdf_reader = PdfReader(f)
-    for page_number in range(len(pdf_reader.pages)):
-        page = pdf_reader.pages[page_number]
-        print(page.extract_text)
-
-print('Successful.')
-'''
 from camelot import read_pdf
 file_name = input('Enter the file name:')
 tables = read_pdf(file_name)
